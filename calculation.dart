81a686e10cf0007604f12f77b550daf2b3f06271
shihab365 <shihabcse@yahoo.com>
2022-02-02 16:23:50 +0600

    addition, subtraction, multiplication, division

---
+++ calculation.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  double a = 10;
+  double b = 5;
+  double result;
+
+  result = a+b;
+  print("Addition: ${result}");
+
+  result = a-b;
+  print("Subtraction: ${result}");
+
+  result = a*b;
+  print("Multiplication: ${result}");
+
+  result = a/b;
+  print("Division: ${result}");
+
+}
