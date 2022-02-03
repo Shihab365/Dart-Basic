b0c13bb1a46bbf707ff025e49974773518dda979
shihab365 <shihabcse@yahoo.com>
2022-02-03 16:39:39 +0600

    length, keys, values method in Map

---
+++ map_method.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  var mMap = {
+    "ID":"1020",
+    "Name":"Charles",
+    "Faculty":"FSIT",
+    "Dept":"ETE",
+    "CGPA":"3.50"
+  };
+
+  print(mMap.length);
+  print(mMap.keys);
+  print(mMap.values);
+  
+}
