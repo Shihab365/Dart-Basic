b3479aab7137e0d30c3160ef272c23c558dfbbfe
shihab365 <shihabcse@yahoo.com>
2022-02-03 14:03:07 +0600

    growable length list

---
+++ growable_list.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  var growListString=<String>[];
+  var growListInteger=<int>[];
+
+  growListString.add("Apple");
+  growListString.add("Orange");
+  growListString.add("Mango");
+  growListString.add("Lemon");
+  growListString.add("Strawberry");
+  print(growListString);
+
+  growListInteger.add(10);
+  growListInteger.add(30);
+  growListInteger.add(20);
+  growListInteger.add(50);
+  growListInteger.add(40);
+  print(growListInteger);
+
+
+}
