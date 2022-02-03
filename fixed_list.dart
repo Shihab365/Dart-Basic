9f674ba46098b504da23aab9d7d730e4a969b592
shihab365 <shihabcse@yahoo.com>
2022-02-03 13:52:14 +0600

    fixed length list

---
+++ fixed_list.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  var mListString = List<String>.filled(3, '');
+  var mListInteger = List<int>.filled(3, 0);
+
+  mListString[0]=("Bus");
+  mListString[1]=("Car");
+  mListString[2]=("Train");
+
+  mListInteger[0]=50;
+  mListInteger[1]=100;
+  mListInteger[2]=150;
+
+  print(mListString);
+  print(mListInteger);
+
+}
