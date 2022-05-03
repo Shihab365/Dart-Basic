41bc69c542a13e857a601ae38a046c68ae850629
shihab365 <shihabcse@yahoo.com>
2022-05-03 17:22:17 +0600

    interface basic

---
+++ interface_basic.dart
@@ -0,0 +1,27 @@
+void main(){
+
+}
+
+class normalClass implements interfaceClass{
+  @override
+  void apple() {
+    
+  }
+
+  @override
+  void banana() {
+    
+  }
+
+  @override
+  void orange() {
+    
+  }
+
+}
+
+class interfaceClass{
+  void apple(){}
+  void orange(){}
+  void banana(){}
+}
